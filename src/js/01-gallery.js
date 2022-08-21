ad0e01e8dc8910f08a9c649a01797171513f56ce
MarynaYa <yamarina30@gmail.com>
2022-08-21 19:09:21 +0300

    dz71-modal-commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,12 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 //console.log(galleryItems);
-const galleryItemsAll = document.querySelector(`.gallery`);
-// Создаем карточку_______________________
-function createImageCards(images) {
+
+
+//// Создаем карточку_______________________
+function createImageCards(galleryItems) {
     return galleryItems.map(({preview, original, description}) => {
-`<div class="gallery__item">
+return `<div class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
     <img
       class="gallery__image"
@@ -17,5 +17,45 @@ function createImageCards(images) {
   </a>
 </div>`;
 }).join(``);
+}
+//console.log(createImageCards(galleryItems));
 
+////Залить разметку в div class="gallery"_______________
+const galleryItemsAll = document.querySelector(`.gallery`);
+const cardsMarkup = createImageCards(galleryItems);
+galleryItemsAll.insertAdjacentHTML(`beforeend`, cardsMarkup);
+let instance = null;
+
+////Делегируем  на div.gallery__________________
+galleryItemsAll.addEventListener(`click`, onContainerClick);
+
+function onContainerClick(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains(`gallery__image`)) { // если элемент на который не содержит класс "gallery__image", то выходим из функции, а если есть то продолжаем выполнять основной код
+    return;
+}
+ const currentImageUrl = event.target.dataset.source;  // доступ к ориг.карточки
+ instance = basicLightbox.create(
+    `<img class="modal__image" src="${currentImageUrl}" />`
+  );
+ instance.show();
+}
+//// Модальное окно_______________
+       // ф-ция откр модального окна
+function onModalOpen() {
+    window.addEventListener(`keydown`, onKeyPress); // вкл слушателя на window 
+}
+    // ф-ция закр модал окна
+function onModalClose() {
+    window.addEventListener(`keydown`, onKeyPress); // вкл слушателя на window
+}
+     // ф-ция собітия
+function onKeyPress(event) { 
+    const ESC_KEY_CODE = `Escape`;                     // вешаем возможность закр при нажатии ESC
+     const isKeyCode = event.code === ESC_KEY_CODE;
+
+    if (isKeyCode) {
+        instance.remove(() => onModalClose);
+    }
 }
