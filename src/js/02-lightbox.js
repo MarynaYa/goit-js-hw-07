caf008aedcd4806d54adcce2ad6f965b5b39ee94
MarynaYa <yamarina30@gmail.com>
2022-08-23 18:47:42 +0300

    dz2-caption-commit

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -5,23 +5,23 @@ console.log(galleryItems);
 const galleryItemsAll = document.querySelector(`.gallery`);
 console.log(galleryItemsAll);
 //// Создаем карточку_______________________
-function createImageCards(galleryItems) {
-    return galleryItems.map(({preview, original, description}) => {
-return `<a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}" />
-  </a>`;
-}).join(``);
-}
+function createImageCards(array) {
+    return array
+      .map(
+        ({ preview, original, description }) =>
+        `<a class="gallery__item" href=${original}>
+    <img class="gallery__image" src=${preview} alt="${description}" />
+  </a>`, 
+      )
+      .join('');
+  }
+  
 ////Залить в галлерею_______________
 const cardsMarkup = createImageCards(galleryItems);
 galleryItemsAll.insertAdjacentHTML(`afterbegin`, cardsMarkup);
 
 // Добавляем путь к подписи и тайминг______________
-var lightbox = new SimpleLightbox('.gallery a', { 
+const lightbox = new SimpleLightbox('.gallery a', {   // данный код скопиррован с библиотеки
+    captionsData: 'alt',
     captionDelay: 250,
-    captionData: "alt",
- });
+  });
